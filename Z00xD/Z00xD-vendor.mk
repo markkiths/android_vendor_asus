0ffe90d6e34f9bcb74b6b6ef63a4ade7757403db
Roker2 <dimaminko2000@gmail.com>
2020-03-22 22:27:40 +0300

    Z00xD: Remove hals.conf
    msm8916-common has it.

--- Z00xD/Z00xD-vendor.mk
+++ Z00xD/Z00xD-vendor.mk
@@ -471,7 +471,6 @@ PRODUCT_COPY_FILES += \
     vendor/asus/Z00xD/proprietary/vendor/lib64/libcalmodule_gyroscope.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcalmodule_gyroscope.so\
     vendor/asus/Z00xD/proprietary/lib/libcmdthread.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcmdthread.so\
     vendor/asus/Z00xD/proprietary/lib64/libcmdthread.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcmdthread.so\
-    vendor/asus/Z00xD/proprietary/etc/sensors/hals.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/sensors/hals.conf\
     vendor/asus/Z00xD/proprietary/vendor/etc/calmodule.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/calmodule.cfg
 	
 # Keymaster
